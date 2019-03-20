ee8fedad16ad59c58ef9f08218063a50a6ce4f84
huangdijia <huangdijia@gmail.com>
2019-03-20 14:09:23 +0800

    support lumen

---
+++ src/LumenController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Huangdijia\JsonRpc;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
+{
+    final public function __invoke()
+    {
+        if (!($this instanceof \Huangdijia\JsonRpc\Controller)) {
+            throw new \Exception(get_class($this) . " must instanceof " . \Huangdijia\JsonRpc\Controller::class, 1);
+        }
+
+        $id     = request()->input('id', 1);
+        $method = request()->input('method', 'index');
+        $params = request()->input('params', []);
+
+        if (!is_callable($this, $method)) {
+            throw new \Exception("Method '{$method}' is not callable!", 1);
+        }
+
+        return call_user_func_array([$this, $method], $params);
+    }
+}
